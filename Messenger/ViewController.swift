0e12de5e7617164c85856f89b813887bdcdb4866
Jihoi-Kang <jihoi.kang@gmail.com>
2021-04-24 01:14:31 +0900

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jihoi Kang on 2021/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
