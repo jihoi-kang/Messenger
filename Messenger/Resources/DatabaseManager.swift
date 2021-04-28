4d04757a09a54d6d764667255115d961939e9d87
Jihoi-Kang <jihoi.kang@gmail.com>
2021-04-28 21:01:52 +0900

    Upload photos to firebase storage

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -35,11 +35,19 @@ extension DatabaseManager {
     }
     
     // Inserts new user to database
-    public func insertUser(with user: ChatAppUser) {
+    public func insertUser(with user: ChatAppUser, completion: @escaping (Bool) -> Void) {
         database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName,
-        ])
+        ], withCompletionBlock: { error, _ in
+            guard error == nil else {
+                print("failed to write to database")
+                completion(false)
+                return
+            }
+            
+            completion(true)
+        })
     }
 }
 
@@ -54,5 +62,8 @@ struct ChatAppUser {
         return safeEmail
     }
     
-    //    let profilePictureUrl: String
+    var profilePictureFileName: String {
+        // /images/jay-gmail-com_profile_picture.png
+        return "\(safeEmail)_profile_picture.png"
+    }
 }
